fd0c6702b64c2bc48b37e9dd4b0a63d6a30a4013
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-08-27 22:20:37 -0600

    Add image for murray movies to murray objects in helper add promise all to ensure all promises are resolved

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,14 +1,20 @@
-import { murrayURL } from './variables'
+import { murrayURL, urlToFetchImage, key } from './variables'
 import { fetchMurrayMovies } from './apiCalls'
 
 export const cleanMurrayData = async () => {
-  // const fetchedUncleanMurrayMovies = await fetchMurrayMovies(murrayURL)
-  // console.log(fetchedUncleanMurrayMovies.results)
+  const fetchedUncleanMurrayMovies = await fetchMurrayMovies(murrayURL)
+  console.log(fetchedUncleanMurrayMovies.results)
   const response = await fetch(murrayURL)
   const uncleanMurrayData = await response.json()
-  return uncleanMurrayData.results.map(result => {
-    console.log(result.title)
-    return {title: result.title, overview: result.overview, release: result.release_date}
+
+  const murrayPromises = uncleanMurrayData.results.map(async result => {
+    const image = urlToFetchImage + result.poster_path
+    console.log(image)
+    return {title: result.title, overview: result.overview, release: result.release_date, image}
   })
+
+  return Promise.all(murrayPromises)
 }
+
+
 
