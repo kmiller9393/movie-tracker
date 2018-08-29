133aaf57ce80509619eba4d5cca161eceacc9b4e
Kurt Miller <kmiller9393@gmail.com>
2018-08-28 21:23:23 -0600

    Add key to MovieContainer li element

--- src/containers/MovieContainer.js
+++ src/containers/MovieContainer.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-
 const MovieContainer = ({ movies }) => {
-    const displayMovies = movies.map(movie => (
-      <li>
-        <img src={movie.image} />
-      </li>)
-  )
-    return(
-      <ul>
-        {displayMovies}
-      </ul>
-  )
-}
+  const displayMovies = movies.map((movie, index) => (
+    <li key={index}>
+      <img src={movie.image} />
+    </li>
+  ));
+  return <ul>{displayMovies}</ul>;
+};
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   movies: state.movies
-})
+});
 
-export default connect(mapStateToProps)(MovieContainer)
+export default connect(mapStateToProps)(MovieContainer);
