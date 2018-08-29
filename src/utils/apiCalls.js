b2a98b878124635d27bbd8bd90c1b987fe8469fc
Kurt Miller <kmiller9393@gmail.com>
2018-08-29 16:07:56 -0600

    Add setUserData method to apiCalls file so users can sign in

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,28 +1,48 @@
-import { key } from './variables'
-
+import { key } from './variables';
 
 export const fetchMurrayMovies = async () => {
-  const uncleanMurrayData = []
+  const uncleanMurrayData = [];
   for (let i = 1; i < 5; i++) {
-    const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=' + key + '&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=' + i +'&with_people=1532')
-    const fetchedMurrayData = await response.json()
-    uncleanMurrayData.push(fetchedMurrayData)
+    const response = await fetch(
+      'https://api.themoviedb.org/3/discover/movie?api_key=' +
+      key +
+      '&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=' +
+      i +
+      '&with_people=1532'
+    );
+    const fetchedMurrayData = await response.json();
+    uncleanMurrayData.push(fetchedMurrayData);
   }
   return uncleanMurrayData;
-}
+};
 
 export const fetchUserData = async (email, password) => {
   try {
     const response = await fetch('http://localhost:3000/api/users', {
       method: 'POST',
-      body: JSON.stringify({ password, email}),
+      body: JSON.stringify({ password, email }),
       headers: {
         'Content-Type': 'application/json'
       }
     });
     const result = await response.json();
-    return result
-  } catch(error) {
-    alert('Invalid Email or Password')
+    return result;
+  } catch (error) {
+    alert('Invalid Email or Password');
   }
-}
+};
+
+export const setUserData = async (name, email, password) => {
+  try {
+    const response = await fetch('http://localhost:3000/api/users/new', {
+      method: 'POST',
+      body: JSON.stringify({ name, password, email }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const result = await response.json();
+  } catch (error) {
+    alert('Email already exists');
+  }
+};
