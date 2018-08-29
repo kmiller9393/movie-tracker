c062c0ec843b540c2871de176c12849161c38727
Kurt Miller <kmiller9393@gmail.com>
2018-08-29 16:05:00 -0600

    Add SignUp component for initial user setup

---
+++ src/components/SignUp.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, Link, NavLink, withRouter } from 'react-router-dom';
+import { setUserData } from '../utils/apiCalls';
+
+class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    await setUserData(this.state.name, this.state.email, this.state.password);
+    this.props.history.push('/');
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="name"
+          value={this.state.name}
+          onChange={this.handleChange}
+          placeholder="Enter Name"
+          type="text"
+        />
+        <input
+          name="email"
+          value={this.state.email}
+          onChange={this.handleChange}
+          placeholder="Enter Email"
+          type="text"
+        />
+        <input
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          placeholder="Password"
+          type="text"
+        />
+        <button>SIGN UP</button>
+      </form>
+    );
+  }
+}
+
+export default withRouter(SignUp);
