665dc0b9e7554e5cba6531355022aa669a359990
Kurt Miller <kmiller9393@gmail.com>
2018-08-30 14:15:15 -0600

    Replace li element with Card component and move setFavoriteData function to Card component

--- src/containers/MovieContainer.js
+++ src/containers/MovieContainer.js
@@ -1,38 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Card from '../components/Card';
 import { connect } from 'react-redux';
 import { logoutUser } from '../actions';
-import { toggleFavorite } from '../actions';
-import { addMovieToDatabase, deleteMovieFromDatabase } from '../utils/apiCalls'
 
-const MovieContainer = ({ movies, user, logoutUser, handleToggle, favorites }) => {
-  console.log(user)
-  const displayMovies = movies.map((movie, index) => (
-    <li key={index}>
-      <button onClick={() => setFavoriteData(movie)}>Favorite</button>
-      <img src={movie.image} />
-    </li>
+const MovieContainer = ({
+  movies,
+  user,
+  logoutUser,
+  handleToggle,
+  favorites
+}) => {
+  const displayMovies = movies.map(movie => (
+    <Card
+      {...movie}
+      key={movie.id}
+      movie={movie}
+      user={user}
+      favorites={favorites}
+    />
   ));
-
-  const setFavoriteData = async (movie) => {
-    if (!favorites.includes(movie)) {
-      handleToggle(movie)
-      await addMovieToDatabase(user, movie)
-    } else {
-      handleToggle(movie)
-      await deleteMovieFromDatabase(user, movie)
-    }
-  }
 
   return (
     <div>
-      {user.name ? <header>Welcome { user.name }<button onClick={logoutUser}> Sign Out </button> </header> : 
-    <header>Welcome
-      <a href="http://localhost:3001/login">Login</a>
-      <a href="http://localhost:3001/sign-up">Sign Up</a>
-    </header>}
-    <ul>{displayMovies}</ul>
+      {user.name ? (
+        <header>
+          Welcome {user.name}
+          <button onClick={logoutUser}> Sign Out </button>{' '}
+        </header>
+      ) : (
+        <header>
+          Welcome
+          <a href="http://localhost:3001/login">Login</a>
+          <a href="http://localhost:3001/sign-up">Sign Up</a>
+        </header>
+      )}
+      <ul>{displayMovies}</ul>
     </div>
-  )
+  );
 };
 
 export const mapStateToProps = state => ({
@@ -41,9 +45,11 @@ export const mapStateToProps = state => ({
   favorites: state.favorites
 });
 
-export const mapDispatchToProps = (dispatch) => ({
-  logoutUser: () => dispatch(logoutUser()),
-  handleToggle: (movie) => dispatch(toggleFavorite(movie))
-})
+export const mapDispatchToProps = dispatch => ({
+  logoutUser: () => dispatch(logoutUser())
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(MovieContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MovieContainer);
