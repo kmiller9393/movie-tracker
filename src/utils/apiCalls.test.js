8e7669e42d3e1b9a73344de7212f8e48db7eb575
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-01 16:17:30 -0600

    Test delete from database function

--- src/utils/apiCalls.test.js
+++ src/utils/apiCalls.test.js
@@ -2,14 +2,36 @@ import { deleteMovieFromDatabase } from './apiCalls'
 import { mockMurrayData, mockMovie, mockUser } from './mockData/mockMurrayData'
 
 describe('apiCalls', () => {
+  let response = {"status":"success","message":"2 row was deleted."}
   beforeEach(() => {
-    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({status:200, json:() => Promise.resolve(mockMurrayData)}))
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({json:() => Promise.resolve(response)}))
   })
   describe('deleteMovieFromDatabase', () => {
-    it.only('should delete a movie from the database', async () => {
-      const result = await deleteMovieFromDatabase(mockUser, mockMovie)
-      console.log(result)
+    it.only('should call fetch fetch with the correct params', () => {
+const url = `http://localhost:3000/api/users/${mockUser.id}/favorites/${mockMovie.id}`
+      
+      const expected = [
+        url, {
+          method: 'DELETE',
+      body: JSON.stringify({user_id: mockUser.id, movie_id: mockMovie.id}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+        }
+      ]
+      deleteMovieFromDatabase(mockUser, mockMovie)
+      // console.log(result)
+      expect(window.fetch).toHaveBeenCalledWith(...expected)
 
     })
   })
 })
+
+// const url = `http://localhost:3000/api/users/${user.id}/favorites/${movie.id}`
+//     await fetch(url, {
+//       method: 'DELETE',
+//       body: JSON.stringify({user_id: user.id, movie_id: movie.id}),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     })
