b1c68e3504cc494567299083a8e295a373833eab
Jesse McFadden <jessemcbrennan@gmail.com>
2018-09-02 15:11:32 -0600

    Add toggle state tests

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -19,11 +19,39 @@ describe('Card', () => {
     expect(wrapper).toMatchSnapshot();
   })
 
+  it('should change state of toggle when the image is clicked', () => {
+  wrapper.find('img').simulate('click')
+  expect(wrapper.state().toggle).toEqual(true)
+  wrapper.find('img').simulate('click')
+  expect(wrapper.state().toggle).toEqual(false)
+  })
+
+  it('should change the state of toggle when the article is clicked', () => {
+    wrapper.find('article').simulate('click')
+    expect(wrapper.state().toggle).toEqual(true)
+    wrapper.find('article').simulate('click')
+    expect(wrapper.state().toggle).toEqual(false)
+  })
+
+  it('should call setFavorieData when clicked', () => {
+    wrapper = mount(<Card movie={mockMovie} image={mockMovie.image} favorites={[]} user={user} handleToggle={jest.fn()} />)
+    wrapper.instance().setFavoriteData = jest.fn()
+    wrapper.find('button').simulate('click')
+
+    expect(wrapper.instance().setFavoriteData).toHaveBeenCalledWith(mockMovie)
+  })
+
   describe('setFavoriteData', async () => {
-    it.only('should call handleToggle with a movie that is passed in', () => {
+    it('should call handleToggle with a movie that is passed in', () => {
       wrapper = mount(<Card movie={mockMovie} image={mockMovie.image} favorites={[]} user={user} handleToggle={jest.fn()} />)
       wrapper.instance().setFavoriteData(mockMovie);
       expect(wrapper.props().handleToggle).toHaveBeenCalledWith(mockMovie)
+    })
+    it('should call history.push with the correct parameters if no user is signed in', () => {
+      wrapper = mount(<Card movie={mockMovie} image={mockMovie.image} favorites={[]} user={user} handleToggle={jest.fn()} />)
+
+      console.log(wrapper.props().history)
+
     })
   })
 })
