ef251b2740561ca13a257f146c28e3fdc6dcaaa2
Jesse McFadden <jessemcbrennan@gmail.com>
2018-09-02 14:40:56 -0600

    Add setFavoritesData test

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { mockMurrayData, mockMovie, mockUser, mockFavorites, mockFavoritesArray } from '../utils/mockData/mockMurrayData'
 import { addFavoriteToDatabase, deleteMovieFromDatabase } from '../utils/__mocks__/apiCalls'
 import  { Card }  from './Card';
@@ -9,8 +9,8 @@ describe('Card', () => {
   let wrapper;
   let user;
   beforeEach(() => {
-    const user = {name: 'Kurt', password: 'Kurt'};
-    wrapper = shallow(<Card movie={mockMovie} image={mockMovie.image} favorites={mockFavoritesArray} user={user} handleToggle={jest.fn()} />)
+    user = {name: 'Kurt', password: 'Kurt'};
+    wrapper = shallow(<Card movie={mockMovie} image={mockMovie.image} favorites={[]} user={user} handleToggle={jest.fn()} />)
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({json:() => Promise.resolve({status: 200})}))
 
   })
@@ -20,8 +20,10 @@ describe('Card', () => {
   })
 
   describe('setFavoriteData', async () => {
-    it.only('should add a movie to the favorites array if the movie is not already included in the favorites array', () => {
+    it.only('should call handleToggle with a movie that is passed in', () => {
+      wrapper = mount(<Card movie={mockMovie} image={mockMovie.image} favorites={[]} user={user} handleToggle={jest.fn()} />)
       wrapper.instance().setFavoriteData(mockMovie);
+      expect(wrapper.props().handleToggle).toHaveBeenCalledWith(mockMovie)
     })
   })
 })
