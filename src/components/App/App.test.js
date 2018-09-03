34909b392f284878f3dd1801ebf1bff2612aaa58
Kurt Miller <kmiller9393@gmail.com>
2018-09-03 16:14:41 -0600

    Add react context to App test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,37 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {shallow, mount} from 'enzyme';
+import createRouterContext from 'react-router-test-context';
+import { shallow, mount } from 'enzyme';
 import { App, mapDispatchToProps } from './App';
-import { cleanMurrayData } from '../../utils/__mocks__/helper'
-import { fetchMurrayMovies } from '../../utils/__mocks__/apiCalls'
-// jest.mock('../../utils/__mocks__/apiCalls');
+import { cleanMurrayData } from '../../utils/__mocks__/helper';
+import { fetchMurrayMovies } from '../../utils/__mocks__/apiCalls';
 
 describe('App', () => {
   let wrapper;
-  beforeEach( async () => {
-   wrapper = await shallow(<App getMovies={jest.fn()}/>)
+  beforeEach(async () => {
+    const context = createRouterContext();
+    wrapper = await shallow(<App getMovies={jest.fn()} />, { context });
     window.fetch = jest
       .fn()
       .mockImplementation(() =>
         Promise.resolve({ json: () => Promise.resolve(fetchMurrayMovies) })
       );
-  })
-  
-  it('should call getMovies from props with the correct params', async () => {
+  });
 
-    const result = await wrapper.instance().fetchMurrayData()
-    console.log(result)
-    expect(cleanMurrayData).toHaveBeenCalledWith('')
-  })
+  it('should call getMovies from props with the correct params', async () => {
+    const result = await wrapper.instance().fetchMurrayData();
+    expect(cleanMurrayData).toHaveBeenCalledWith('');
+  });
 
   describe('mapDispatchToPros', () => {
     it('should call dispatch when getMovies from props is invoked', () => {
       const mockDispatch = jest.fn();
 
-      const mappedProps = mapDispatchToProps(mockDispatch)
+      const mappedProps = mapDispatchToProps(mockDispatch);
       mappedProps.getMovies();
-      expect(mockDispatch).toHaveBeenCalled()
-    })
-  })
-
-})
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+  });
+});
